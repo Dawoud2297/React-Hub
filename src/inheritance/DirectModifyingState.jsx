096e8e424f3324469f625e6440f75502ef2ea57c
Mahmoud-Dawood <soluman2297@gmail.com>
2023-10-02 10:27:02 +0300

    Update DirectModifyingState.jsx

--- src/inheritance/DirectModifyingState.jsx
+++ src/inheritance/DirectModifyingState.jsx
@@ -4,19 +4,6 @@ export default class DirectModifyingState extends Component {
     state = {
         counter: 0
     }
-    // First Method
-    // startCounter  = () =>{
-    //     this.setState({
-    //         counter : this.state.counter + 1
-    //     })
-    // }
-    // Second Method
-    // startCounter = () => {
-    //     this.setState((state) => ({
-    //         counter: state.counter + 1
-    //     }))
-    // }
-    // Third Method
     startCounter = () => {
         this.setState((state) => {
             return { counter: state.counter + 1 }
