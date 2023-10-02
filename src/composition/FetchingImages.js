c6f19b36de31e70f6ecd0f40277f0c77d71fb107
Mahmoud-Dawood <soluman2297@gmail.com>
2023-10-02 10:24:40 +0300

    Update FetchingImages.js

--- src/composition/FetchingImages.js
+++ src/composition/FetchingImages.js
@@ -3,11 +3,6 @@ import React from 'react'
 const FetchingImages = ({ image }) => {
     return (
         <div>
-            {/* First Resource */}
-            {/* <img alt='image0' src='logo192.png'/> */}
-            {/* Second Resource */}
-            {/* <img alt='image1' src='/images/lifecycle_reactjs.jpg'/> */}
-            {/* <img alt='image2' src={'images/' + image} /> */}
             <img alt='image3' src={`images/${image}`}/>
         </div>
     )
