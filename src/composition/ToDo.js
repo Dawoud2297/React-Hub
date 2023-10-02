bea790b3eab0d160d9c8cd24489326e227a11f22
Mahmoud-Dawood <soluman2297@gmail.com>
2023-10-02 10:25:28 +0300

    Update ToDo.js

--- src/composition/ToDo.js
+++ src/composition/ToDo.js
@@ -5,16 +5,6 @@ const ToDo = () => {
   return (
     <div>
       <input type="text"placeholder='Message object'value={messageObject.message}onChange={e=>{
-        // First implementation Wrong
-        // messageObject.message = e.target.value;
-        // setMessage(messageObject)
-        // Second implementation Wrong
-        // { messageObject } = e.target.value
-        // setMessage(messageObject)
-        // Third implementation Right
-        // const newMessage = {message : e.target.value}
-        // setMessage(newMessage)
-        // Fourth implementation Right
         setMessage({
           message : e.target.value
         })
